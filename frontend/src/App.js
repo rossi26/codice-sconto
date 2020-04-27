9aa5ae3b0e413cf2dfb1f6f8c64c4e1e30fde53c
lorenzo <rossi.26.mario@gmail.com>
2020-04-27 20:45:14 +0200

    login persit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,49 @@ class App extends Component{
     commId:null,
     
   }
+
+  componentDidMount() {
+    /* console.log("App.componentDidMount: refreshing, then authenticating.."); */
+    this.refreshToken().then(result => {
+      console.log(result)
+        if (result) {
+
+          let base64Url = result.split(".")[1];
+    
+    
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
+   
+    const tokenData = JSON.parse(atob(base64));
+           /*  console.log("App.componentDidMount: the user is authenticated"); */
+               this.setState({
+              token:result,
+              commId:tokenData.commId,
+              
+            }) 
+        } else {
+            /* console.log("App.componentDidMount: the user is not authenticated"); */
+        }
+    });
+}
+
+refreshToken = () => {
+const t=localStorage.getItem("tokens")
+console.log(t)
+ /*  const tokens = JSON.parse(localStorage.getItem("tokens"));
+  console.log(tokens) */
+  if (t !== null) {
+    
+
+    /*   console.log("App.refreshToken: access token not expired, nothing to do."); */
+      return Promise.resolve(t)
+   
+  }else{
+  /* console.log("App.refreshToken: no tokens found in localStorage."); */
+  return Promise.resolve(false);}
+};
+
   login=(token, commId, tokenExpiration)=>{
+    localStorage.setItem("tokens", token)
     this.setState({
       token:token,
       commId:commId,
@@ -24,6 +66,7 @@ class App extends Component{
 
   }
   logout=()=>{
+    localStorage.removeItem("tokens");
     this.setState({
       token:null,
       commId:null
